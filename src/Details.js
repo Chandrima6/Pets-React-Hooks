0f82b03ad15574d8f2d7bf004306d9be3b3d458f
chandrimadasgupta <c.dasgupta@bboxx.co.uk>
2020-06-08 11:08:57 +0100

    created image carousel and added details

--- src/Details.js
+++ src/Details.js
@@ -1,11 +1,44 @@
 import React from "react";
+import Pet from "@frontendmasters/pet";
+import Carousel from "./Carousel";
 
-const Details = (props) => {
-  return (
-    <pre>
-      <code>{JSON.stringify(props, null, 4)}</code>
-    </pre>
-  );
-};
+class Details extends React.Component {
+  state = {
+    loading: true,
+  };
+
+  componentDidMount() {
+    Pet.animal(this.props.id).then(({ animal }) => {
+      this.setState({
+        id: animal.id,
+        name: animal.name,
+        type: animal.type,
+        breed: animal.breeds,
+        description: animal.description,
+        location: `${animal.contact.address.city}, ${animal.contact.address.state}`,
+        media: animal.photos,
+        loading: false,
+      });
+    }, console.error);
+  }
+
+  render() {
+    if (this.state.loading) {
+      return <h1>loading...</h1>;
+    }
+    const { name, type, breed, description, location, media } = this.state;
+    return (
+      <div className="details">
+        <Carousel media={media} />
+        <div>
+          <h1>{name}</h1>
+          <h2>{`${type} - ${breed.primary} - ${location}`}</h2>
+          <button>Adopt {name}</button>
+          <p>{description}</p>
+        </div>
+      </div>
+    );
+  }
+}
 
 export default Details;
